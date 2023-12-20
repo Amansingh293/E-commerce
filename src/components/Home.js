3fc4f9363bc49edd2705f94c39d335c897989eb2
Amansingh293 <amansinghdl9c@gmail.com>
2023-12-21 02:20:44 +0530

    navbar color change

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,29 +6,35 @@ import { add } from "../Slices/cart";
 import Card from "./Card";
 
 export default function Home() {
-
   const [products, setProducts] = useState([]);
 
   let dispatch = useDispatch();
   // console.log(dispatch);
-  const addHandler = (product)=>{
+  const addHandler = (product) => {
     dispatch(add(product));
-  } 
+  };
 
   const getProducts = async () => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setProducts(json));
+      .then((json) => setProducts(json))
+      .catch((err) => console.log(err.message));
   };
 
   useEffect(() => {
     getProducts();
   }, []);
 
-
+  console.log(products);
   return (
     <div className="cardContainer">
-      {(products.length===0) ? <div style={{fontSize: "29px"}}>Loading....</div> : products.map((product) =><Card product = {product} addHandler = {addHandler}/>) }
+      {products.length === 0 ? (
+        <div style={{ fontSize: "29px" }}>Loading....</div>
+      ) : (
+        products.map((product) => (
+          <Card product={product} addHandler={addHandler} />
+        ))
+      )}
     </div>
   );
 }
